eca23965f21a37de745e2394bda154e612ecf794
Darole111 <86237686+Darole111@users.noreply.github.com>
2021-06-22 13:12:32 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,20 @@
+
 package exam;
 
 public class ConvertToBinary {
 
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
-  }
+	public String toBinary(final int n) {
+		// TODO write your code here to achieve the desired result as explained in
+		// Readme file for this problem.
+		String result = "";
+		int num = n;
+		while (num > 0) {
+			if ((num % 2) == 0)
+				result = "0" + result;
+			else
+				result = "1" + result;
+			num = num / 2;
+		}
+		return result;
+	}
 }
