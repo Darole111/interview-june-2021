82eb852c4f99ea226d3f7ff2bc193a0244d9be8f
Darole111 <86237686+Darole111@users.noreply.github.com>
2021-06-22 13:14:07 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -1,10 +1,22 @@
 package exam;
 
+import java.util.Arrays;
+
 public class ShiftElementsInArray {
 
   public void shift(int[] arr) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-
+	  int[] reverse=new int[arr.length];
+	  
+	  for(int i=0;i<arr.length;i++)
+	  {
+		  if(i<(arr.length-1)) 
+			  reverse[i+1]=arr[i];
+		  else 
+			  reverse[0]=arr[i];
+	  }
+	  
+	  System.out.println(Arrays.toString(reverse));
   }
 
 }
